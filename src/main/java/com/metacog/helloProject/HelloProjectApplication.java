1a0cee8cb17ab11c2fa00130f55956e37ff8bffe
metacog <metacog@kakao.com>
2022-04-24 18:57:41 +0900

    gradle build

---
+++ src/main/java/com/metacog/helloProject/HelloProjectApplication.java
@@ -0,0 +1,13 @@
+package com.metacog.helloProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloProjectApplication.class, args);
+	}
+
+}
